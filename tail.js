70d95deb15ae8a7a4646ccdd1ece4eece3401cb2
k2pbac <krisbachan123@hotmail.com>
2021-08-16 15:20:48 -0400

    add tail function, change equality checking

---
+++ tail.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let equal = false;
+
+  for (let el of actual) {
+    if (!expected.includes(el)) {
+      equal = false;
+    } else {
+      equal = true;
+    }
+  }
+
+  if (equal === false) {
+    console.log(`😡😡😡 Assertion Failed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+
+};
+const tail = (arr) => {
+  return arr.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
