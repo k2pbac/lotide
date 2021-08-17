c3140fa78a59d1e6792192219709be929ad0333d
k2pbac <krisbachan123@hotmail.com>
2021-08-17 13:54:08 -0400

    add test case

--- eqArrays.js
+++ eqArrays.js
@@ -21,7 +21,7 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
