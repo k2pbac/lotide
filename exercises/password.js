47c0e419ecd8347441a8731d632b0c78ece5628a
k2pbac <krisbachan123@hotmail.com>
2021-08-23 16:18:31 -0400

    add index.js file to contain all modules

--- exercises/password.js
+++ exercises/password.js
@@ -30,4 +30,6 @@ const obfuscatePassword = () => {
   return strongPass;
 };
 
-console.log(obfuscatePassword());
+module.exports = {
+  obfuscatePassword
+}
---
+++ index.js
@@ -0,0 +1,34 @@
+// index.js
+const head   = require('./exercises/head');
+const tail   = require('./exercises//tail');
+const middle = require('./exercises//middle');
+const check = require("./exercises/check");
+const countLetters = require("./exercises/countLetters");
+const countOnly = require("./exercises/countOnly");
+const findKey = require("./exercises/findKey");
+const findKeyByValue = require("./exercises/findKeyByValue");
+const flatten = require("./exercises/flatten");
+const letterPositions = require("./exercises/letterPositions");
+const map = require("./exercises/map");
+const obfuscatePassword = require("./exercises/password");
+const takeUntil = require("./exercises/takeUntil");
+const without = require("./exercises/without");
+
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  check,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  obfuscatePassword,
+  takeUntil,
+  without
+};
