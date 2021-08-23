aad4cc0f3ce4c435726b8597f0db32bbd31339f5
k2pbac <krisbachan123@hotmail.com>
2021-08-23 15:53:00 -0400

    add chai and mocha to head tests

--- test/headTest.js
+++
@@ -1,9 +0,0 @@
-const {assertEqual }= require("../exercises/assertEqual");
-const {head} = require("../exercises/head");
-
-assertEqual(head([5,6,7]), 5); // returns true
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // returns true
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp"); // returns false
-assertEqual(1, 1); // returns true; 
